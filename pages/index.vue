6d6444256810e436440418ea1cbc77b87ff66b1a
Georges <georges.kabbouchi@gmail.com>
2022-12-12 16:12:44 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ const { safeAddress } = useAvocadoSafe()
         </div>
 
         <div class="md:flex space-between gap-5">
-            <Balances class="w-full mb-5 md:mb-0" />
+            <Balances class="w-full mb-5 md:mb-0 md:pb-14" />
 
             <div class="md:w-56 flex-shrink-0 space-y-5">
                 <QrCode />
