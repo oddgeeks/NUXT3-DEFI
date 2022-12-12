595956e0eae7897535f55c4a0c0a4ccd94050d29
Georges <georges.kabbouchi@gmail.com>
2022-12-12 16:29:40 +0200

    hide gas

--- components/Web3Button.vue
+++ components/Web3Button.vue
@@ -92,7 +92,7 @@ const closeConnection = () => {
   </CommonModal>
   <button v-show="(active && !buttonOnly)" @click="closeConnection"
     class="w-full bg-slate-800 text-white rounded-[30px] text-white inline-flex items-center justify-between px-4 py-1.5 gap-3.5 ">
-    <div class="flex items-center space-x-2">
+    <div v-if="false" class="flex items-center space-x-2">
       <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M0 10C0 4.47715 4.47715 0 10 0H15C17.7614 0 20 2.23858 20 5V10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10Z"
@@ -108,7 +108,8 @@ const closeConnection = () => {
 
     <span class="flex items-center justify-between gap-3.5 font-semibold">
 
-      <div class="flex flex-row items-center gap-2.5 py-1.5 px-3 bg-slate-600 rounded-[30px] leading-5">
+      <!-- bg-slate-600 -->
+      <div class="flex flex-row items-center gap-2.5 py-1.5 px-3  rounded-[30px] leading-5">
         {{ shortenHash(account) }}
       </div>
 
