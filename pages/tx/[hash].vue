0068dcdb0e70ee4440f43f12f58775c28d064d79
Georges <georges.kabbouchi@gmail.com>
2022-12-16 19:31:25 +0200

    add tx page

---
+++ pages/tx/[hash].vue
@@ -0,0 +1,24 @@
+<script setup>
+const router = useRoute()
+const provider = getRpcProvider(420)
+
+
+const [avoInternalTransaction, tansaction, receipt] = await Promise.all([
+    provider.send("api_getTransactionByHash", [
+        router.params.hash
+    ]),
+    provider.getTransaction(router.params.hash),
+    provider.getTransactionReceipt(router.params.hash),
+])
+
+</script>
+
+<template>
+    <div class="container">
+        <pre class="overflow-scroll">{{ avoInternalTransaction }}</pre>
+
+        <pre class="overflow-scroll">{{ tansaction }}</pre>
+        
+        <pre class="overflow-scroll">{{ receipt }}</pre>
+    </div>
+</template>
