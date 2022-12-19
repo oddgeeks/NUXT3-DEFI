6a422b2e8413b251ce00c1d9e4db05217550110d
oddgeeks <oddgeeks.dev@gmail.com>
2022-12-19 21:57:12 +0300

    feat: init wallet connect section

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,14 @@ const { safeAddress } = useAvocadoSafe();
 </script>
 
 <template>
-  <div class="container">
-    <div>
+  <div class="container flex flex-col gap-[30px]">
       <TotalBalance />
-    </div>
-
-    <div class="flex gap-5 lg:flex-row flex-col">
-      <Balances class="mb-5 md:mb-0 md:pb-14 flex-1" />
-
-      <div class="flex flex-col md:basis-[213px] gap-5">
-        <QrCode />
-        <WalletConnect />
+      <DApps />
+      <div class="flex gap-5 lg:flex-row flex-col">
+        <Balances class="mb-5 md:mb-0 md:pb-14 flex-1" />
+        <div class="flex flex-col md:basis-[213px] gap-5">
+          <QrCode />
+        </div>
       </div>
     </div>
-  </div>
 </template>
