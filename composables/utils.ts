7b3be89ff421e11521c8aa55e7f603113c13d65d
oddgeeks <oddgeeks.dev@gmail.com>
2022-12-24 23:50:10 +0300

    fix: add fallback

--- composables/utils.ts
+++ composables/utils.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
-import axios from 'axios'
-import axiosRetry from 'axios-retry'
+import axios from "axios";
+import axiosRetry from "axios-retry";
 import { RPC_URLS } from "~~/connectors";
 
 export function shortenHash(hash: string, length: number = 4) {
@@ -12,40 +12,49 @@ export function shortenHash(hash: string, length: number = 4) {
   return shortened;
 }
 
-const rpcInstances: Record<string, ethers.providers.JsonRpcProvider> = {}
+const rpcInstances: Record<string, ethers.providers.JsonRpcProvider> = {};
 
 export const getRpcProvider = (chainId: number | string) => {
   if (!rpcInstances[chainId]) {
-    rpcInstances[chainId] = new ethers.providers.JsonRpcProvider(RPC_URLS[Number(chainId)])
+    rpcInstances[chainId] = new ethers.providers.JsonRpcProvider(
+      RPC_URLS[Number(chainId)]
+    );
   }
 
-  return rpcInstances[chainId]
-}
+  return rpcInstances[chainId];
+};
 
-import { BigNumber } from "bignumber.js"
-import { BigNumber as BN } from "@ethersproject/bignumber"
+import { BigNumber } from "bignumber.js";
+import { BigNumber as BN } from "@ethersproject/bignumber";
 
-export const toBN = (value: BigNumber.Value | BN) => new BigNumber(
-  BN.isBigNumber(value) ? value.toString() : value
-)
-export const isZero = (value: BigNumber.Value | BN) => toBN(value).isZero()
-export const times = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).times(toBN(b))
-export const minus = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).minus(toBN(b))
-export const plus = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).plus(toBN(b))
-export const lte = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).lte(toBN(b))
-export const gte = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).gte(toBN(b))
-export const div = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).div(toBN(b))
-export const lt = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).lt(toBN(b))
-export const gt = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) => toBN(a).gt(toBN(b))
+export const toBN = (value: BigNumber.Value | BN) =>
+  new BigNumber(BN.isBigNumber(value) ? value.toString() : value);
+export const isZero = (value: BigNumber.Value | BN) => toBN(value).isZero();
+export const times = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).times(toBN(b));
+export const minus = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).minus(toBN(b));
+export const plus = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).plus(toBN(b));
+export const lte = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).lte(toBN(b));
+export const gte = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).gte(toBN(b));
+export const div = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).div(toBN(b));
+export const lt = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).lt(toBN(b));
+export const gt = (a: BigNumber.Value | BN, b: BigNumber.Value | BN) =>
+  toBN(a).gt(toBN(b));
 export const ensureValue = (value: any) => {
-  if (!value) return toBN('0')
-  if (toBN(value).isNaN()) return toBN('0')
+  if (!value) return toBN("0");
+  if (toBN(value).isNaN()) return toBN("0");
 
-  return toBN(value)
-}
+  return toBN(value);
+};
 export const max = (...args: BigNumber.Value[]) => {
-  return BigNumber.max(...args)
-}
+  return BigNumber.max(...args);
+};
 export const chainIdToName = (chainId: string | number) => {
   switch (String(chainId)) {
     case "1":
@@ -66,7 +75,6 @@ export const chainIdToName = (chainId: string | number) => {
       return "Mainnet";
   }
 };
-
 
 export const getExplorerUrl = (
   chainId: string | number,
@@ -99,38 +107,35 @@ export function formatUsd(value: any, fractionDigits = 2) {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: fractionDigits,
-    maximumFractionDigits: fractionDigits
+    maximumFractionDigits: fractionDigits,
   });
 
   return formatter.format(value);
 }
 
-export function formatDecimal(
-  value: any,
-  fractionDigits = 6
-) {
+export function formatDecimal(value: any, fractionDigits = 6) {
   let formatter;
   if (lt(value, "0.000001") && gt(value, "0")) {
     formatter = new Intl.NumberFormat(locale, {
       style: "decimal",
       notation: "scientific",
       minimumFractionDigits: 2,
-      maximumFractionDigits: 2
+      maximumFractionDigits: 2,
     });
   } else {
     formatter = new Intl.NumberFormat(locale, {
       style: "decimal",
       minimumFractionDigits: fractionDigits,
-      maximumFractionDigits: fractionDigits
+      maximumFractionDigits: fractionDigits,
     });
   }
 
-  return formatter.format(value);
+  return formatter.format(value || 0);
 }
 
-export const http = axios.create()
+export const http = axios.create();
 
 axiosRetry(http, {
   retries: 3,
   retryDelay: axiosRetry.exponentialDelay,
-})
+});
