46deefb26737f20a47f413200b2d355dd76b9228
oddgeeks <oddgeeks.dev@gmail.com>
2022-12-29 09:33:41 +0300

    fix: text update

--- pages/index.vue
+++ pages/index.vue
@@ -5,27 +5,32 @@ const { safeAddress } = useAvocadoSafe();
 </script>
 
 <template>
-    <div class="container flex flex-col gap-[30px] flex-1 md:pb-10">
-      <TotalBalance />
-      <DApps />
-      <h2 class="font-semibold -mb-2.5 inline-flex gap-2.5">Balances
-        <button @click="openDialogModal({
-          type:'question',
-          title: 'AvoSafe Balances',
-          isButtonVisible: false,
-          content:`These are your AvoSafe Balances, not your EOA balances. Deposit funds into your AvoSafe to use begin using Avocado.
+  <div class="container flex flex-col gap-[30px] flex-1 md:pb-10">
+    <TotalBalance />
+    <DApps />
+    <h2 class="font-semibold -mb-2.5 inline-flex gap-2.5">
+      Balances
+      <button
+        @click="
+          openDialogModal({
+            type: 'question',
+            title: 'AvoSafe Balances',
+            isButtonVisible: false,
+            content: `These are your AvoSafe Balances, not your EOA balances. Deposit funds into your AvoSafe to begin using Avocado.
           <br><br>
           <a href='https://instadapp.io' target='blank' rel='noopener noreferrer' class='text-sm inline-flex gap-2.5 text-blue-500'>Learn more about how to deposit ${LinkSVG}</a>
           `,
-        })">
-          <QuestionCircleSVG class="w-5 h-5" />
-        </button>
-      </h2>
-      <div class="flex gap-5 lg:flex-row flex-col flex-1">
-        <Balances />
-        <div class="flex flex-col md:basis-[213px] gap-5">
-          <QrCode />
-        </div>
+          })
+        "
+      >
+        <QuestionCircleSVG class="w-5 h-5" />
+      </button>
+    </h2>
+    <div class="flex gap-5 lg:flex-row flex-col flex-1">
+      <Balances />
+      <div class="flex flex-col md:basis-[213px] gap-5">
+        <QrCode />
       </div>
     </div>
+  </div>
 </template>
