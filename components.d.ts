bb16be70c9ddf4de90e33afd411a51aa9ab0ee97
oddgeeks <oddgeeks.dev@gmail.com>
2023-01-08 19:27:25 +0300

    fix: stroke issue

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,10 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Listbox: typeof import('@headlessui/vue')['Listbox']
+    ListboxButton: typeof import('@headlessui/vue')['ListboxButton']
+    ListboxOption: typeof import('@headlessui/vue')['ListboxOption']
+    ListboxOptions: typeof import('@headlessui/vue')['ListboxOptions']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TransitionChild: typeof import('@headlessui/vue')['TransitionChild']
