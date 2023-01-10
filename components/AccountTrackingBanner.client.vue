98703e2fef6dd6c1dccad0d54ef246f15a15fd07
oddgeeks <oddgeeks.dev@gmail.com>
2023-01-10 16:34:51 +0300

    fix: conditional rendering

---
+++ components/AccountTrackingBanner.client.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const router = useRouter();
+const { trackingAccount } = useAccountTrack();
+
+const disconnect = () => {
+  trackingAccount.value = null;
+  router.go(0);
+};
+</script>
+
+<template>
+    <div
+      class="w-full h-9 bg-blue-500 bg-opacity-10 shrink-0 flex justify-center items-center gap-[15px]"
+    >
+      <p class="text-xs text-blue-500">
+        Tracking account: {{ shortenHash(trackingAccount) }}
+      </p>
+      <button @click="disconnect" class="text-red-alert text-xs">
+        (Disconnect)
+      </button>
+    </div>
+</template>
