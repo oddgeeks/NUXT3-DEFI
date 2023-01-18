b2bd9d840badab7e49af5a51a2f5356a6fd5a66e
Georges <georges.kabbouchi@gmail.com>
2023-01-19 00:54:26 +0200

    hide tooltips on scroll

--- app.vue
+++ app.vue
@@ -9,16 +9,26 @@ const { tried } = useEagerConnect();
 whenever(tried, () => {
   useAccountTrack();
 })
+
+onMounted(() => {
+  const hideAllTooltipsOnScroll = useThrottleFn(() => {
+    [...document.querySelectorAll("[data-tippy-root]")].forEach((e) => e._tippy?.hide())
+  }, 1000)
+
+  document.addEventListener("scroll", hideAllTooltipsOnScroll, true)
+
+  return () => document.removeEventListener("scroll", hideAllTooltipsOnScroll)
+})
 </script>
 
 <template>
-    <NuxtLayout>
-      <NuxtLoadingIndicator color="#3b82f6" height="2" />
-      <NuxtPage />
-    </NuxtLayout>
+  <NuxtLayout>
+    <NuxtLoadingIndicator color="#3b82f6" height="2" />
+    <NuxtPage />
+  </NuxtLayout>
 
-    <Modal />
-    <Notifications />
+  <Modal />
+  <Notifications />
 </template>
 
 
