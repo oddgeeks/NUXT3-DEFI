34eb7a106d13f9418fa34b900adb58a9dc7d6487
oddgeeks <oddgeeks.dev@gmail.com>
2023-02-03 11:12:37 +0300

    fix: change bridge icon

--- components/modals/PendingTransaction.vue
+++ components/modals/PendingTransaction.vue
@@ -1,75 +1,135 @@
 <script setup lang="ts">
+import SVGBridge from "~/assets/images/icons/bridge.svg?component";
 import SVGCheckCircle from "~/assets/images/icons/check-circle.svg?component";
-import { TransactionReceipt } from '@ethersproject/abstract-provider';
-import { wait } from '@instadapp/utils';
+import { TransactionReceipt } from "@ethersproject/abstract-provider";
+import { wait } from "@instadapp/utils";
 
-const props = defineProps<{ hash: string, chainId: number | string, type: ITxType }>()
-const provider = getRpcProvider(props.chainId)
-const transaction = ref<TransactionReceipt>()
+const props = defineProps<{
+  hash: string;
+  chainId: number | string;
+  type: ITxType;
+}>();
+const provider = getRpcProvider(props.chainId);
+const transaction = ref<TransactionReceipt>();
 
 onMounted(async () => {
   await wait(5000);
 
-  transaction.value = await provider.waitForTransaction(props.hash)
-})
+  transaction.value = await provider.waitForTransaction(props.hash);
+});
 
 const txnInfos = computed(() => {
-  if (props.type === 'bridge') {
+  if (props.type === "bridge") {
     return {
       url: `https://socketscan.io/tx/${props.hash}`,
-      text: 'View Bridge Explorer'
-    }
+      text: "View Bridge Explorer",
+    };
   } else {
     return {
       url: getExplorerUrl(props.chainId, `/tx/${props.hash}`),
-      text:  `View Explorer`
-    }
+      text: `View Explorer`,
+    };
   }
-})
-
+});
 </script>
 
 <template>
   <div class="text-center flex flex-col gap-7.5">
     <div v-if="transaction" class="flex justify-center">
       <SVGCheckCircle
-      v-if="transaction.status"
-      class="text-white w-10 h-10 success-circle"/>
+        v-if="transaction.status"
+        class="text-white w-10 h-10 success-circle"
+      />
 
-      <svg v-else width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        v-else
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <rect width="40" height="40" rx="20" fill="#EB5757" />
-        <path d="M25 15L15 25" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M15 15L25 25" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+        <path
+          d="M25 15L15 25"
+          stroke="white"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M15 15L25 25"
+          stroke="white"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
       </svg>
-
     </div>
 
-    
     <div v-if="!transaction" class="flex justify-center">
-      <svg class="animate-spin h-10 w-10 text-[#89BA4F]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      <svg
+        class="animate-spin h-10 w-10 text-[#89BA4F]"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
       </svg>
     </div>
 
     <div>
-      <h2 class="text-lg leading-5">Transaction {{ transaction ? (transaction.status ? "Confirmed" : "Failed") : "Pending" }}
-    </h2>
+      <h2 class="text-lg leading-5">
+        Transaction
+        {{
+          transaction
+            ? transaction.status
+              ? "Confirmed"
+              : "Failed"
+            : "Pending"
+        }}
+      </h2>
 
-    <p v-if="!transaction" class="text-slate-400 text-xs leading-5">The transaction has been sent to be processed.</p>
+      <p v-if="!transaction" class="text-slate-400 text-xs leading-5">
+        The transaction has been sent to be processed.
+      </p>
 
-    <p v-if="transaction && !transaction.status" class="text-slate-400 text-xs">Try again or return to the home page.</p>
+      <p
+        v-if="transaction && !transaction.status"
+        class="text-slate-400 text-xs"
+      >
+        Try again or return to the home page.
+      </p>
     </div>
 
-    
-     <div class="bg-slate-800 px-[18px] py-[14px] rounded-5 flex items-center justify-between">
-      <p class="flex gap-3 items-center">
-        <ChainLogo class="w-[26px] h-[26px]" :chain="chainId"/>
+    <div
+      class="bg-slate-800 px-[18px] py-[14px] rounded-5 flex items-center justify-between"
+    >
+      <p v-if="type === 'bridge'" class="flex gap-3 items-center">
+        <div class="bg-blue-500 rounded-full">
+          <SVGBridge class="w-[26px] h-[26px]" />
+        </div>
+        <span class="text-sm text-slate-400">Bridge</span>
+      </p>
+      <p v-else class="flex gap-3 items-center">
+        <ChainLogo class="w-[26px] h-[26px]" :chain="chainId" />
         <span class="text-sm text-slate-400">{{ chainIdToName(chainId) }}</span>
       </p>
-      <CommonButton as="a" :href="txnInfos.url" target="_blank" size="sm" >
-       {{ txnInfos.text }}
+      <CommonButton as="a" :href="txnInfos.url" target="_blank" size="sm">
+        {{ txnInfos.text }}
       </CommonButton>
-     </div>
+    </div>
   </div>
 </template>
