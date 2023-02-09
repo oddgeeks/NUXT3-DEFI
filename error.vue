32510cc6ded030ee2dabc5477274a758ee24c1ed
oddgeeks <oddgeeks.dev@gmail.com>
2023-02-09 12:08:23 +0300

    fix: update 404

--- error.vue
+++ error.vue
@@ -17,15 +17,8 @@ const { parseRequestError } = useErrorHandler()
 
     <main class="h-full flex flex-col">
       <div class="container relative flex-1 flex justify-center">
-        <Svg404 class="absolute -z-10 pointer-events-none inset-0" />
         <div class="flex flex-col justify-center items-center text-center max-w-3xl mx-auto">
-          <img
-            class="mb-[60px]"
-            style="width: 280px; height: 280px"
-            width="280"
-            height="280"
-            src="/sad_logo.png"
-          />
+         <h1 style="font-size: 256px;" class="leading-none mb-8">404</h1>
          <div class="flex flex-col gap-[26px] mb-10">
           <h1 class="text-[40px] leading-10">{{ parseRequestError(error).statusMessage }}</h1>
           <h2 class="text-slate-400 leading-[30px]">{{ parseRequestError(error).message }}</h2>
