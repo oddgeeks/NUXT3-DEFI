a91f983ace7f0f10d58b62e6d06dcec7e2e76443
JenyaFTW <yyakovliev02@gmail.com>
2023-02-21 20:50:26 +0200

    wip: confetti

--- pages/claims/ens-drop.vue
+++ pages/claims/ens-drop.vue
@@ -1,13 +1,39 @@
 <script setup lang="ts">
 import confetti from "canvas-confetti";
 import { ethers } from "ethers";
+import CheckCircle from "~/assets/images/icons/check-circle.svg?component";
 
 const { parseTransactionError } = useErrorHandler();
 const { account, library } = useWeb3();
 const eligible = ref(false);
+const claimed = ref(false);
 
-watch(eligible, eligible => {
-  if (!eligible) return;
+const claimedConfetti = () => {
+  fire(1, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(1, {
+    spread: 60,
+  });
+  fire(1, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
+  fire(1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
+  fire(1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+};
+
+const eligibleConfetti = () => {
   fire(0.25, {
     spread: 26,
     startVelocity: 55,
@@ -30,6 +56,11 @@ watch(eligible, eligible => {
     spread: 120,
     startVelocity: 45,
   });
+};
+
+watch(eligible, eligible => {
+  if (!eligible) return;
+  eligibleConfetti();
 });
 
 watch(account, account => {
@@ -38,12 +69,11 @@ watch(account, account => {
   eligible.value = true;
 });
 
-const count = 500;
 const defaults = {
   origin: { y: 0.6 }
 };
 
-const fire = (particleRatio: any, opts: any) => {
+const fire = (particleRatio: any, opts: any, count: number = 200) => {
   confetti(Object.assign({}, defaults, opts, {
     particleCount: Math.floor(count * particleRatio)
   }));
@@ -75,6 +105,9 @@ const claimAirdrop = async () => {
       redeemSignature,
       airdropNonce,
     ]);
+
+    claimedConfetti();
+    claimed.value = true;
     
     if (success) {
       fire(0.25, {
@@ -127,8 +160,8 @@ useForceSingleSession();
       </div>
     </div>
     <div 
-      v-else-if="eligible"
-      class="flex flex-col items-center gap-7.5 bg-gray-850 p-12 rounded-5"
+      v-else-if="eligible && !claimed"
+      class="flex flex-col items-center gap-7.5 bg-slate-50 dark:bg-gray-850 p-12 rounded-5"
     >
       <span class="w-[320px] text-center text-slate-400">You are eligible! You have received</span>
       <span class="text-6xl">50 USDC</span>
@@ -136,11 +169,23 @@ useForceSingleSession();
         Claim
       </CommonButton>
     </div>
-    <div v-else class="flex flex-col items-center gap-7.5">
+    <div v-else-if="!claimed" class="flex flex-col items-center gap-7.5 bg-slate-50 dark:bg-gray-850 p-12 rounded-5">
       <span class="w-[360px] text-center">You are not eligible! or Try again with another ENS.</span>
       <CommonButton size="lg">
         <span class="text-center w-full">Back</span>
       </CommonButton>
+    </div>
+    <div v-else class="flex flex-col gap-7.5 bg-slate-50 dark:bg-gray-850 p-12 rounded-5">
+      <span class="text-left text-lg">What you can do on Avocado with your claimed gas</span>
+      <div class="flex flex-col gap-5">
+        <div 
+          v-for="ability in ['Deposit tokens to your avo wallet first', 'Connect your favorite dapps and making txns using USDC', 'Swap tokens', 'Bridge tokens']"
+          class="flex items-center gap-2"
+        >
+          <CheckCircle class="text-white w-4 h-4 success-circle"/>
+          <span>{{ ability }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
