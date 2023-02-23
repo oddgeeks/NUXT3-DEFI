126d1e68428b27da3a424c139e39c274ddb27f7c
oddgeeks <oddgeeks.dev@gmail.com>
2023-02-23 17:18:13 +0300

    fix: improve UI

--- pages/upgrade.vue
+++ pages/upgrade.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const { account } = useWeb3();
 const { data } = useNuxtData("allNetworkVersions");
 </script>
 
@@ -13,27 +14,46 @@ const { data } = useNuxtData("allNetworkVersions");
         keep your Smart Contract upgraded to make the most of Avocado.
       </h2>
     </div>
-    <div class="dark:bg-gray-850 bg-slate-50 rounded-[25px] flex-1">
-      <table class="table w-full">
-        <thead>
-          <tr
-            class="text-left text-sm text-gray-400 font-medium border-b border-slate-150 dark:border-slate-800"
-          >
-            <th class="text-left py-6 pl-7.5">Network</th>
-            <th class="py-5">Latest version</th>
-            <th class="py-5 text-center">
-              <span class="opacity-0">Action</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody class="divide-y dark:divide-slate-800 divide-slate-150">
-          <NetworkUpgradeRow
-            :network="network"
-            :key="network.chainId"
-            v-for="network in data"
-          />
-        </tbody>
-      </table>
+    <div class="h-full relative">
+      <div
+        :class="{ 'blur h-full': !account || !data }"
+        class="dark:bg-gray-850 bg-slate-50 rounded-[25px] flex-1 relative"
+      >
+        <table class="table w-full">
+          <thead>
+            <tr
+              class="text-left text-sm text-gray-400 font-medium border-b border-slate-150 dark:border-slate-800"
+            >
+              <th class="text-left py-6 pl-7.5">Network</th>
+              <th class="py-5">Latest version</th>
+              <th class="py-5 text-center">
+                <span class="opacity-0">Action</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody class="divide-y dark:divide-slate-800 divide-slate-150">
+            <NetworkUpgradeRow
+              :network="network"
+              :key="network.chainId"
+              v-for="network in data"
+            />
+          </tbody>
+        </table>
+      </div>
+      <div
+        v-if="!account"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center"
+      >
+        <div class="flex flex-col items-center justify-center gap-6">
+          <p class="font-semibold text-lg whitespace-nowrap">
+            Connect your wallet to upgrade
+          </p>
+
+          <div class="w-28">
+            <Web3Button />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
