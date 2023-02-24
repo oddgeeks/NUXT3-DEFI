c239c1a3c2502d4a13703e59c332e953c1c654e5
oddgeeks <oddgeeks.dev@gmail.com>
2023-02-24 14:50:17 +0300

    fix: remove _blank

--- pages/tx/[hash].vue
+++ pages/tx/[hash].vue
@@ -105,53 +105,79 @@ onUnmounted(() => {
   <div class="container flex-1 md:pb-10">
     <h1 class="mb-5">Transaction Details</h1>
 
-    <div class="dark:bg-gray-850 bg-slate-50 rounded-5.5 text-sm font-medium py-6.5 min-h-[548px] blur"
-      v-if="!transaction && pending" />
+    <div
+      class="dark:bg-gray-850 bg-slate-50 rounded-5.5 text-sm font-medium py-6.5 min-h-[548px] blur"
+      v-if="!transaction && pending"
+    />
 
-    <div v-if="transaction" class="dark:bg-gray-850 bg-slate-50 rounded-5.5 text-sm font-medium py-6.5">
+    <div
+      v-if="transaction"
+      class="dark:bg-gray-850 bg-slate-50 rounded-5.5 text-sm font-medium py-6.5"
+    >
       <div class="flex flex-col gap-6.5 px-7.5">
         <div class="flex items-center">
-          <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Transaction Hash
           </div>
           <div class="flex items-center space-x-2.5">
-            <a class="text-primary" :href="
-              getExplorerUrl(transaction.chain_id, `/tx/${transaction.hash}`)
-            " target="_blank">{{ transaction.hash }}</a>
+            <a
+              class="text-primary"
+              :href="
+                getExplorerUrl(transaction.chain_id, `/tx/${transaction.hash}`)
+              "
+              target="_blank"
+              >{{ transaction.hash }}</a
+            >
             <Copy :text="transaction.hash" />
           </div>
         </div>
 
         <div class="flex items-center">
-          <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Broadcaster
           </div>
-          <div class="flex items-center space-x-2.5">
-            <a class="text-primary" :href="
-              getExplorerUrl(
-                transaction.chain_id,
-                `/address/${transaction.from}`
-              )
-            " target="_blank">
+          <div v-if="transaction.from" class="flex items-center space-x-2.5">
+            <a
+              class="text-primary"
+              :href="
+                getExplorerUrl(
+                  transaction.chain_id,
+                  `/address/${transaction.from}`
+                )
+              "
+              target="_blank"
+            >
               {{ transaction.from }}
             </a>
             <Copy :text="transaction.from"></Copy>
           </div>
+          <span v-else>-</span>
         </div>
 
         <div class="flex items-center">
-          <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Status
           </div>
           <TransactionStatus :status="transaction.status" />
 
-          <span v-if="transaction.revert_reason" class="ml-2 text-xs text-red-400">
+          <span
+            v-if="transaction.revert_reason"
+            class="ml-2 text-xs text-red-400"
+          >
             ({{ transaction.revert_reason }})
           </span>
         </div>
 
         <div class="flex items-center">
-          <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Timestamp
           </div>
           <div class="capitalize">
@@ -166,7 +192,9 @@ onUnmounted(() => {
 
       <div class="px-7.5 py-6.5">
         <div class="flex items-center">
-          <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Network
           </div>
           <div class="capitalize flex items-center">
@@ -182,25 +210,44 @@ onUnmounted(() => {
 
         <div class="px-7.5 py-6.5 flex gap-6.5 flex-col">
           <div class="flex">
-            <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+            <div
+              class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+            >
               Transaction Action
             </div>
             <div class="flex items-center flex-col gap-6">
-              <ActionMetadata :metadata="item" :key="i" :transaction="transaction" v-for="(item, i) of metadata" />
+              <ActionMetadata
+                :metadata="item"
+                :key="i"
+                :transaction="transaction"
+                v-for="(item, i) of metadata"
+              />
             </div>
           </div>
 
           <div v-if="isBridge" class="flex items-center">
-            <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px] flex items-center gap-2">
+            <div
+              class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px] flex items-center gap-2"
+            >
               Bridge Status
 
-              <SVGInfo v-tippy="
-                'This is a token bridging transaction. Click on the link to check the estimated time for the transaction to be processed.'
-              " class="w-[18px] h-[18px] text-slate-600 shrink-0" />
+              <SVGInfo
+                v-tippy="
+                  'This is a token bridging transaction. Click on the link to check the estimated time for the transaction to be processed.'
+                "
+                class="w-[18px] h-[18px] text-slate-600 shrink-0"
+              />
             </div>
-            <div v-if="bridgeStatusPending && !bridgeStatus" class="rounded-5 w-24 h-4 loading-box" />
-            <a v-else-if="bridgeStatus" class="flex items-center" :href="`https://socketscan.io/tx/${transaction.hash}`"
-              target="_blank">
+            <div
+              v-if="bridgeStatusPending && !bridgeStatus"
+              class="rounded-5 w-24 h-4 loading-box"
+            />
+            <a
+              v-else-if="bridgeStatus"
+              class="flex items-center"
+              :href="`https://socketscan.io/tx/${transaction.hash}`"
+              target="_blank"
+            >
               <TransactionStatus :status="bridgeStatus">
                 <LinkSVG class="w-4" />
               </TransactionStatus>
@@ -214,17 +261,27 @@ onUnmounted(() => {
 
       <div class="px-7.5 py-6.5 flex flex-col gap-6.5">
         <div class="flex items-center">
-          <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Signer
           </div>
 
-          <div v-if="transaction.metadata.signer" class="flex items-center space-x-2.5">
-            <a class="text-primary" :href="
-              getExplorerUrl(
-                transaction.chain_id,
-                `/address/${transaction.metadata.signer}`
-              )
-            " target="_blank">{{ transaction.metadata.signer }}</a>
+          <div
+            v-if="transaction.metadata.signer"
+            class="flex items-center space-x-2.5"
+          >
+            <a
+              class="text-primary"
+              :href="
+                getExplorerUrl(
+                  transaction.chain_id,
+                  `/address/${transaction.metadata.signer}`
+                )
+              "
+              target="_blank"
+              >{{ transaction.metadata.signer }}</a
+            >
 
             <Copy :text="transaction.metadata.signer" />
           </div>
@@ -233,17 +290,27 @@ onUnmounted(() => {
         </div>
 
         <div class="flex items-center">
-          <div class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Avocado Wallet
           </div>
 
-          <div v-if="transaction.metadata.safe" class="flex items-center space-x-2.5">
-            <a class="text-primary" :href="
-              getExplorerUrl(
-                transaction.chain_id,
-                `/address/${transaction.metadata.safe}`
-              )
-            " target="_blank">{{ transaction.metadata.safe }}</a>
+          <div
+            v-if="transaction.metadata.safe"
+            class="flex items-center space-x-2.5"
+          >
+            <a
+              class="text-primary"
+              :href="
+                getExplorerUrl(
+                  transaction.chain_id,
+                  `/address/${transaction.metadata.safe}`
+                )
+              "
+              target="_blank"
+              >{{ transaction.metadata.safe }}</a
+            >
 
             <Copy :text="transaction.metadata.safe" />
           </div>
@@ -256,11 +323,16 @@ onUnmounted(() => {
 
       <div class="px-7.5 flex flex-col pt-6.5 gap-6.5">
         <div class="flex items-center">
-          <div class="dark:text-slate-400 gap-2.5 flex items-center text-slate-500 md:w-full md:max-w-[235px]">
+          <div
+            class="dark:text-slate-400 gap-2.5 flex items-center text-slate-500 md:w-full md:max-w-[235px]"
+          >
             Transaction Fee
-            <SVGInfo v-tippy="
-              'This includes the fee that will be paid to the relayer and the integrator.'
-            " class="w-[18px] h-[18px] text-slate-600 shrink-0" />
+            <SVGInfo
+              v-tippy="
+                'This includes the fee that will be paid to the relayer and the integrator.'
+              "
+              class="w-[18px] h-[18px] text-slate-600 shrink-0"
+            />
           </div>
           <div class="capitalize">
             <span v-if="transaction.fee">
@@ -272,7 +344,10 @@ onUnmounted(() => {
       </div>
     </div>
 
-    <div v-else-if="!transaction && !pending" class="bg-gray-850 rounded-5.5 text-sm font-medium p-16 text-center">
+    <div
+      v-else-if="!transaction && !pending"
+      class="bg-gray-850 rounded-5.5 text-sm font-medium p-16 text-center"
+    >
       <p class="mb-2">Sorry, We are unable to locate this TxnHash:</p>
       <p>{{ $route.params.hash }}</p>
     </div>
