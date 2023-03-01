6f6051bf99014a2b648c3b99d09153063cd7fac9
JenyaFTW <yyakovliev02@gmail.com>
2023-03-01 19:42:55 +0200

    feat: edit gas popup

--- components/warnings/GasBalance.vue
+++ components/warnings/GasBalance.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="bg-orange-400 gap-[15px] w-full justify-center flex bg-opacity-10 text-orange-400 rounded-5 p-4 text-sm text-center">
+    class="bg-green-400 gap-[15px] w-full justify-center flex bg-opacity-10 text-green-400 rounded-5 p-4 text-sm text-center">
     <span class="text-xs self-center">
-      Your gas balance is insufficient. Please top up to trigger transactions.
+      Get started with Avocado by topping up gas using USDC
     </span>
-    <CommonButton class="h-7.5 !px-4 flex items-center text-xs" @click="openTopUpGasModal()">Top up</CommonButton>
+    <CommonButton class="h-7.5 !px-4 flex items-center text-xs" @click="openTopUpGasModal()">Deposit USDC</CommonButton>
   </div>
 </template>
