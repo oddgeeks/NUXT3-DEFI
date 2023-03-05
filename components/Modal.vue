f8499c64bbbe59ae7b0531ed42655b74e029caeb
oddgeeks <oddgeeks.dev@gmail.com>
2023-03-05 13:51:54 +0300

    fix: change breakpoint

--- components/Modal.vue
+++ components/Modal.vue
@@ -66,24 +66,24 @@ whenever(escape, () => {
   >
     <div
       :data-modal-id="modalId"
-      class="flex md:items-center justify-center h-full min-h-screen text-center sm:p-0 modal-inner"
+      class="flex sm:items-center justify-center h-full min-h-screen text-center sm:p-0 modal-inner"
     >
       <div
         :class="options.wrapperClass"
-        class="inline-block w-full mt-auto md:my-6 dark:bg-gray-950 bg-white rounded-t-7.5 md:rounded-7.5 text-left align-middle transition-all transform max-w-[460px]"
+        class="inline-block w-full mt-auto sm:my-6 dark:bg-gray-950 bg-white rounded-t-7.5 sm:rounded-7.5 text-left align-middle transition-all transform max-w-[460px]"
         role="dialog"
         aria-modal="true"
       >
         <div
           ref="wrapperRef"
           :class="options.contentClass"
-          class="modal-content-wrapper rounded-[inherit] relative md:px-[50px] px-6 pb-8 md:py-10 w-full"
+          class="modal-content-wrapper rounded-[inherit] relative sm:px-[50px] px-6 pb-8 sm:py-10 w-full"
         >
           <button
             @touchmove.passive="handleDestory"
             @mousedown="handleDestory"
             type="button"
-            class="w-full justify-center h-9 flex md:hidden"
+            class="w-full justify-center h-9 flex sm:hidden"
           >
             <div
               class="h-1 w-9 dark:bg-slate-600 rounded-[10px] mt-2 bg-slate-300"
@@ -92,7 +92,7 @@ whenever(escape, () => {
             </div>
           </button>
           <button
-            class="absolute h-7.5 w-7.5 rounded-full items-center justify-center hidden md:flex dark:bg-slate-800 bg-slate-100 top-0 right-0 m-6"
+            class="absolute h-7.5 w-7.5 rounded-full items-center justify-center hidden sm:flex dark:bg-slate-800 bg-slate-100 top-0 right-0 m-6"
             @click="handleDestory"
             aria-label="Close modal"
           >
