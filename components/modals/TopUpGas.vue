8b434ff666b21b85b1ea172d8439abaa306b214d
Thrilok <thrilok2000@gmail.com>
2023-03-10 05:19:57 +0400

    Revert "fix: bring back gift"

--- components/modals/TopUpGas.vue
+++ components/modals/TopUpGas.vue
@@ -97,7 +97,6 @@ const sendingDisabled = computed(
     !meta.value.valid
 );
 
-
 const claim = async () => {
   try {
     claimLoading.value = true;
@@ -289,9 +288,6 @@ onMounted(() => {
     >
       {{ formatDecimal(gasBalance, 2) }} USDC
     </span>
-    <CommonButton :loading="claimLoading" @click="claim()" v-if="airDrop?.id" class="flex text-sm items-center gap-2">
-       {{ airDrop.message }}
-    </CommonButton>
     <form v-if="!isGiftActive" @submit="onSubmit" class="space-y-5">
       <div class="flex flex-col gap-2.5">
         <span class="text-left leading-5">Network</span>
