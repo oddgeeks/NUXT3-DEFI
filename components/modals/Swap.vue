9cf6ad24921f39914efc620000bb1da02b8771e1
oddgeeks <oddgeeks.dev@gmail.com>
2023-03-14 14:25:56 +0300

    fix: add label

--- components/modals/Swap.vue
+++ components/modals/Swap.vue
@@ -54,6 +54,7 @@ const { account } = useWeb3();
 
 const slippages = [
   { value: "0.1", label: "0.1%" },
+  { value: "0.3", label: "0.3%" },
   { value: "0.5", label: "0.5%" },
   { value: "1", label: "1%" },
   { value: "2", label: "2%" },
