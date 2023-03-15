021ea764b76fe10c01cf67bad10123ffb95327aa
oddgeeks <oddgeeks.dev@gmail.com>
2023-03-15 16:12:33 +0300

    refactor: add slack log for network errors

--- utils/http.ts
+++ utils/http.ts
@@ -1,5 +1,18 @@
 import { ofetch } from "ofetch";
 
+const logError = useThrottleFn((error) => {
+  logActionToSlack({
+    account: "0x",
+    action: "network",
+    message: `Error
+Request: ${error.request}
+Error: ${error.response._data.error}
+Status: ${error.response._data.statusCode}`,
+    type: "error",
+  });
+}, 1000);
+
 export default ofetch.create({
   retry: 3,
+  onResponseError: (error) => logError(error),
 });
