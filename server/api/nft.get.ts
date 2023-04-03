30b6dde21c1a78cc54812d3f05e66c5ba8500eb3
oddgeeks <oddgeeks.dev@gmail.com>
2023-04-03 22:03:26 +0300

    fix: remove endpoint

--- server/api/nft.get.ts
+++
@@ -1,8 +0,0 @@
-import { AnkrProvider } from "@ankr.com/ankr.js";
-
-const provider = new AnkrProvider();
-
-const nfts = await provider.getNFTsByOwner({
-  walletAddress: "0x32b271D89178724141a644a6efD7fF5512Cd900a",
-  pageSize: 50,
-});
