40fe65e4de5ff95911e84cd3ff2144f6120ecdf5
dxphilo <developerphilo@gmail.com>
2023-04-11 14:16:49 +0300

    fix: remove debug

--- components/modals/Swap.vue
+++ components/modals/Swap.vue
@@ -395,7 +395,9 @@ const { data: txs } = useAsyncData(
       data: bestRoute.value?.data.calldata,
       value: bestRoute.value?.data.value,
     });
-
+    console.log("staring  here");
+    console.log(txs);
+    console.log("finished here");
     return txs;
   },
   {
