7ffa6fd5327fb1a3f25746ef1618c38a8474c51c
oddgeeks <oddgeeks.dev@gmail.com>
2023-04-20 15:50:25 +0300

    rm eslintcache

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,27 +1,25 @@
 interface ISlackParams {
-  title: string;
-  message: string;
-  address: string;
-  chainId: string | number | undefined;
+  title: string
+  message: string
+  address: string
+  chainId: string | number | undefined
 }
 
 // Added slack logs to understand how is exactly balance fetching happening on production
-export const slackIt = async (
-  type: ISlackMessageType,
-  params: ISlackParams
-) => {
-  const { address, message, chainId, title } = params;
+export async function slackIt(type: ISlackMessageType,
+  params: ISlackParams) {
+  const { address, message, chainId, title } = params
 
   const msg = `${title}
-${"`user`"} ${address}
-${"`network`"} ${chainId}
-${"`error`"}  ${message}`;
+${'`user`'} ${address}
+${'`network`'} ${chainId}
+${'`error`'}  ${message}`
 
-  $fetch("/api/slack", {
-    method: "POST",
+  $fetch('/api/slack', {
+    method: 'POST',
     body: {
-      type: type,
+      type,
       message: msg,
     },
-  });
-};
+  })
+}
