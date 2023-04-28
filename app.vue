89bade2844f604e7940e3886f8a177ecfba30ebd
oddgeeks <oddgeeks.dev@gmail.com>
2023-04-28 14:23:03 +0300

    test

--- app.vue
+++ app.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('Service Worker registered:', registration)
+      console.log('Service Worker registeredd:', registration)
     }).catch((error) => {
       console.log('Service Worker registration failed:', error)
     })
