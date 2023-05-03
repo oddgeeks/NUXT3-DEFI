dc49c590397416f8044b3ecc8d549df1a4f937f8
oddgeeks <oddgeeks.dev@gmail.com>
2023-05-03 17:09:32 +0300

    feat: implement version prompt

--- app.vue
+++ app.vue
@@ -22,18 +22,11 @@ onMounted(() => {
 
 <template>
   <NuxtLayout>
-    <ClientOnly>
-      <div v-if="$pwa.needRefresh" class="flex items-center gap-2.5 fixed right-4 bottom-0">
-        <button @click="$pwa.updateServiceWorker(true)">
-          Refresh
-        </button>
-        <SVGInfo class="w-[18px] h-[18px] shrink-0" />
-      </div>
-    </ClientOnly>
     <NuxtLoadingIndicator color="#16A34A" :height="2" />
     <NuxtPage />
   </NuxtLayout>
 
+  <BannerNewVersion />
   <Notifications />
   <Modals />
 </template>
---
+++ components/banner/NewVersion.client.vue
@@ -0,0 +1,29 @@
+<template>
+  <Transition name="slide-fade">
+    <div v-if="$pwa.needRefresh" class="flex items-center gap-[15px] sm:max-w-[400px] w-full bg-[#4CA054] dark:bg-[#4CA054] dark:bg-opacity-60 bg-opacity-30 backdrop-blur px-5 rounded-5 pr-7.5 py-4 fixed left-1/2 transform -translate-x-1/2 bottom-[50px]">
+      <SvgoGear />
+      <p class="text-xs leading-5 flex-1 dark:text-white text-slate-900">
+        New version available
+      </p>
+      <CommonButton @click="$pwa.updateServiceWorker(true)">
+        Reload
+      </CommonButton>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  bottom: -20px;
+  opacity: 0;
+}
+</style>
