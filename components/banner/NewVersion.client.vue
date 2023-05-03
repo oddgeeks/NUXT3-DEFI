27d8209530757d1cedbef52f0c202fb5377044bb
oddgeeks <oddgeeks.dev@gmail.com>
2023-05-03 17:11:24 +0300

    test

--- components/banner/NewVersion.client.vue
+++ components/banner/NewVersion.client.vue
@@ -3,7 +3,7 @@
     <div v-if="$pwa.needRefresh" class="flex items-center gap-[15px] sm:max-w-[400px] w-full bg-[#4CA054] dark:bg-[#4CA054] dark:bg-opacity-60 bg-opacity-30 backdrop-blur px-5 rounded-5 pr-7.5 py-4 fixed left-1/2 transform -translate-x-1/2 bottom-[50px]">
       <SvgoGear />
       <p class="text-xs leading-5 flex-1 dark:text-white text-slate-900">
-        New version available
+        New version available.
       </p>
       <CommonButton @click="$pwa.updateServiceWorker(true)">
         Reload
