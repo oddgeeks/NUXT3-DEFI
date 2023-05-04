e22c7c4f01fa1a22ce62aee32c2b06d9b67a67fb
oddgeeks <oddgeeks.dev@gmail.com>
2023-05-04 11:01:22 +0300

    refactor: add stable scrollbar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -24,7 +24,7 @@ const [moreOptions, toggleOptions] = useToggle(false)
 
 <template>
   <ClientOnly>
-    <aside class="hidden sm:flex overflow-y-auto scroll-style h-screen dark:bg-gray-850 bg-slate-50 transition-all" :class="{ 'w-[340px]': opened, 'w-[120px]': !opened }">
+    <aside style="scrollbar-gutter:stable;overflow-y:overlay;" class="hidden sm:flex overflow-y-auto scroll-style h-screen dark:bg-gray-850 bg-slate-50 transition-all" :class="{ 'w-[340px]': opened, 'w-[120px]': !opened }">
       <div v-if="opened" class="flex flex-col w-full">
         <div class="flex flex-col border-b-1 dark:border-slate-750 border-slate-150 gap-6 pt-7.5 pb-6 px-7.5">
           <div class="flex items-center justify-between">
