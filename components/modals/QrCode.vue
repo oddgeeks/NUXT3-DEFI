ec2cb403a67331328b21b1a4fb1026d74196bdd2
oddgeeks <oddgeeks.dev@gmail.com>
2023-05-06 00:05:33 +0300

    refactor: qr modal

--- components/modals/QrCode.vue
+++ components/modals/QrCode.vue
@@ -5,22 +5,22 @@ const account = computed(() => safeAddress.value || '0x000000000000000')
 
 <template>
   <div
-    class="relative flex flex-col items-center gap-4 w-[400px]"
+    class="relative flex flex-col items-center gap-4 w-[420px]"
     :class="{ 'blur pointer-events-none': !safeAddress }"
   >
     <StyledQrCode
       :key="account"
-      class="rounded-10 bg-white overflow-hidden"
-      :size="400"
+      class="rounded-10 mt-5 bg-white overflow-hidden"
+      :size="420"
       :margin="20"
       :data="account"
     />
 
-    <div class="relative bg-slate-50 dark:bg-gray-850 rounded-7.5 flex flex-col justify-center items-center gap-2.5 px-7.5 py-4 overflow-hidden w-full">
+    <div class="relative bg-slate-50 dark:bg-gray-850 rounded-7.5 flex flex-col justify-center items-center gap-2.5 px-4 py-4 overflow-hidden w-full">
       <span class="text-sm dark:text-slate-500 text-slate-400">Your Avocado Address</span>
-      <Copy :text="account" class="w-full">
+      <Copy :text="account" class="w-full justify-center">
         <template #content>
-          <span class="dark:text-white text-slate-900 text-base break-all">{{ account }}</span>
+          <span class="dark:text-white text-slate-900 text-[14px]">{{ account }}</span>
         </template>
       </Copy>
     </div>
