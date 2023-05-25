9cb9f60044168fb05296ad4e623b1659c443cac4
SchnapsterDog <olivertrajceski@yahoo.com>
2023-05-25 18:03:05 +0200

    feat: created TheAuthHeader component

---
+++ components/TheAuthHeader.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="items-center justify-end py-8 hidden sm:flex">
+    <NuxtLink class="mr-auto" to="/">
+      <Logo />
+    </NuxtLink>
+    <nav class="flex items-center gap-7.5 relative">
+      <div class="flex items-center gap-5">
+        <ColorModeSwitcher />
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
--- layouts/auth.vue
+++ layouts/auth.vue
@@ -5,10 +5,9 @@
   <section class="flex flex-col">
     <div class="flex">
       <div class="flex flex-1 flex-col sm:px-10 px-4 max-w-7xl mx-auto min-w-0">
+        <TheAuthHeader />
         <slot />
-        <div class="mt-top">
-          <TheFooter />
-        </div>
+        <TheFooter />
       </div>
     </div>
   </section>
