193e8fe3d03e13b9d4f3bbdae544596edcc28daf
oddgeeks <oddgeeks.dev@gmail.com>
2023-06-06 13:48:40 +0300

    fix: remove log

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,8 +3,6 @@ export default defineNuxtRouteMiddleware((to) => {
   const shouldExclude = routesToBeExclude.includes(to.fullPath)
   const cachedProviderName = useCookie('cachedProviderName')
 
-  console.log(cachedProviderName.value)
-
   if (!cachedProviderName.value && shouldExclude)
     return navigateTo('/login')
 })
