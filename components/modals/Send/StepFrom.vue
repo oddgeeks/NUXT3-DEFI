8e4704886df63d2111c3446720de9439ba794ab2
oddgeeks <oddgeeks.dev@gmail.com>
2023-06-08 13:21:07 +0300

    fix: enable crosschainsend by query

--- components/modals/Send/StepFrom.vue
+++ components/modals/Send/StepFrom.vue
@@ -9,6 +9,10 @@ const { safeAddress } = useAvocadoSafe()
 
 const { isCrossChain, data, token, availableTokens, toAvailableNetworks, actualAddress, stepForward } = useSend()
 
+const route = useRoute()
+
+const isCrossChainEnabled = computed(() => route.query?.crossChainSend)
+
 const {
   value: amount,
   errorMessage,
@@ -132,7 +136,10 @@ function handleContinue() {
 
     <div class="flex flex-col gap-2.5 font-medium">
       <div class="flex justify-between gap-5">
-        <div class="flex flex-col gap-2.5">
+        <div
+          v-if="isCrossChainEnabled"
+          class="flex flex-col gap-2.5"
+        >
           <span class="text-sm">Network</span>
           <CommonSelect
             v-model="data.toChainId"
