41f033c0f41eca639b3dfcd9194370b59b82d95f
Thrilok <thrilok2000@gmail.com>
2023-06-09 19:59:25 +0530

    added condition for native token transfer

--- components/modals/Send/StepCross.vue
+++ components/modals/Send/StepCross.vue
@@ -206,14 +206,21 @@ async function fetchBestRoute() {
     const approvalData = buildTx.result.approvalData
 
     const transferAmount = toWei(data.value.amount, token.value?.decimals!).toString()
-
+      
     const targetActions = [
-      {
-        to: targetToken.value.address,
-        data: (new ethers.utils.Interface(ERC20ABI)).encodeFunctionData('transfer', [account.value, transferAmount]),
-        operation: '0',
-        value: '0',
-      },
+      targetToken.value.address.toLowerCase() === "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee".toLowerCase() ? 
+        { // Native token transfer
+          to: account.value,
+          data: "0x",
+          operation: '0',
+          value: transferAmount,
+        } :
+        { // ERC20 token transfer
+          to: targetToken.value.address,
+          data: (new ethers.utils.Interface(ERC20ABI)).encodeFunctionData('transfer', [account.value, transferAmount]),
+          operation: '0',
+          value: '0',
+        },
     ]
 
     const sourceActions = [
