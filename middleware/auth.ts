01d9539fbca8ab37357d4d6aeb2ee03797edc05e
oddgeeks <oddgeeks.dev@gmail.com>
2023-06-11 14:11:33 +0300

    fix: ignore tracking account to login page

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,11 @@
 export default defineNuxtRouteMiddleware((to) => {
+  const trackAccount = useCookie<boolean>('trackAccount')
   const cachedProviderName = useCookie('cachedProviderName')
+
+  const trackingUser = to.query?.user || trackAccount.value
 
   const redirectPath = to.path
 
-  if (!cachedProviderName.value)
+  if (!cachedProviderName.value && !trackingUser)
     return navigateTo(`/login?redirectTo=${redirectPath}`)
 })
