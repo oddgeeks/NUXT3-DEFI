c9d3c5837ea2a94f61254a48a1a85a46f1d7b930
oddgeeks <oddgeeks.dev@gmail.com>
2023-06-26 10:16:15 +0300

    fix: ts errors

--- components/common/Toggle.vue
+++ components/common/Toggle.vue
@@ -1,18 +1,13 @@
 <script lang="ts" setup>
-const props = withDefaults(
-  defineProps<{
-    text: string
-    modelValue?: Boolean
-  }>(),
-  {
-    text: '',
-    modelValue: false,
-  },
-)
+defineProps<{
+  text: string
+  modelValue?: boolean
+}>()
 
 const emit = defineEmits(['update:modelValue'])
 function change(ev: Event) {
-  emit('update:modelValue', ev.target.checked)
+  const target = ev.target as HTMLInputElement
+  emit('update:modelValue', target.checked)
 }
 </script>
 
@@ -21,7 +16,7 @@ function change(ev: Event) {
     <span class="ml-3 text-xs font-medium text-gray-900 dark:text-gray-300">{{ text }}</span>
     <label class="relative inline-flex items-center cursor-pointer">
       <input :checked="modelValue" type="checkbox" class="sr-only peer" @change="change">
-      <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary" />
+      <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary" />
     </label>
   </div>
 </template>
