db25dcbe12cfd1bd7c3ea1649178ac28f4fb4152
oddgeeks <oddgeeks.dev@gmail.com>
2023-07-11 09:30:53 +0300

    fix: add failed status

--- components/multisig/PendingTransactionItem.vue
+++ components/multisig/PendingTransactionItem.vue
@@ -8,6 +8,7 @@ const props = defineProps<{
 
 const isConfirmationsMatch = computed(() => props.item.confirmations.length === props.item.confirmations_required)
 const isTransactionExecuted = computed(() => props.item.executed_at !== null)
+const isTransactionFailed = computed(() => props.item.status === 'failed')
 
 const firstActionMetadata = computed<any>(() => {
   const data = decodeMetadata(props.item.data.params.metadata) as string[]
@@ -36,6 +37,7 @@ const formattedActionType = computed(() => {
         <ActionMetadata v-for="metadata in decodeMetadata(item.data.params.metadata)" v-once :key="metadata" compact :chain_id="item.chain_id" :metadata="metadata" />
       </span>
       <span class="whitespace-nowrap">
+        {{ item.status }}
         {{ formatTimeAgo(new Date(activeTab === 'completed' ? item.executed_at : item.created_at)) }}
       </span>
       <span class="flex items-center gap-2.5 w-[140px]">
@@ -44,21 +46,27 @@ const formattedActionType = computed(() => {
           {{ item.confirmations.length }} out of {{ item.confirmations_required }}
         </span>
       </span>
-      <div class="w-56" :class="isConfirmationsMatch ? 'text-primary' : 'text-orange-400'">
-        <span v-if="isTransactionExecuted" class="flex items-center gap-2">
-          Executed
-          <SvgoCheckCircle class="success-circle w-5 h-5" />
-        </span>
-        <span v-else-if="isConfirmationsMatch" class="items-center flex gap-10 justify-between">
-          Ready to execute
-          <SvgoCheckCircle class="success-circle w-5 h-5" />
-        </span>
-        <span v-else class="items-center flex gap-10 justify-between">
-          Awaiting confirmations
-          <span class="w-5 h-5 flex items-center justify-center">
-            <SvgoHourGlass class="w-4 h-4" />
+      <div class="w-56">
+        <div :class="isConfirmationsMatch ? 'text-primary' : 'text-orange-400'">
+          <span v-if="isTransactionFailed" class="flex items-center text-red-alert gap-2">
+            Failed
+            <SvgoErrorCircle class="text-white w-5 h-5" />
           </span>
-        </span>
+          <span v-else-if="isTransactionExecuted" class="flex items-center gap-2">
+            Executed
+            <SvgoCheckCircle class="success-circle w-5 h-5" />
+          </span>
+          <span v-else-if="isConfirmationsMatch" class="items-center flex gap-10 justify-between">
+            Ready to execute
+            <SvgoCheckCircle class="success-circle w-5 h-5" />
+          </span>
+          <span v-else class="items-center flex gap-10 justify-between">
+            Awaiting confirmations
+            <span class="w-5 h-5 flex items-center justify-center">
+              <SvgoHourGlass class="w-4 h-4" />
+            </span>
+          </span>
+        </div>
       </div>
     </button>
   </li>
