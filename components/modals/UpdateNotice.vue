865ce97a1e9ddfb287356b92ad1a3d6f5fbdd85e
mastercodercat <shinyhenryhan@gmail.com>
2023-07-20 17:53:05 +0200

    Mobile responsive fixed for Update Notice modal

--- components/modals/UpdateNotice.vue
+++ components/modals/UpdateNotice.vue
@@ -19,18 +19,18 @@ watch(lastNoticeShowDate, () => {
 
 <template>
   <div class="flex flex-col justify-center items-center">
-    <p>
+    <p class="text-[18px] leading-[25px] font-semibold">
       Update Avocado
     </p>
-    <p class="mt-5 text-slate-400 text-center text-sm">
+    <p class="mt-[12px] text-slate-400 text-center text-[12px] leading-[20px]">
       New Avocado version available. Please update to a newer version to ensure seamless experience & access to latest features.
     </p>
-    <div class="w-full mt-8 flex flex-row items-center justify-center gap-2">
-      <CommonButton color="white" class="flex-1 justify-center text-sm" @click="onRemindLater()">
+    <div class="w-full mt-[30px] flex flex-col-reverse md:flex-row items-center justify-center gap-[16px] md:gap-2">
+      <CommonButton color="white" class="w-full md:flex-1 justify-center text-sm rounded-full" @click="onRemindLater()">
         Remind me later
       </CommonButton>
-      <CommonButton class="flex-1 justify-center text-sm" @click="onUpdateBtn()">
-        Update now
+      <CommonButton class="w-full md:flex-1 justify-center text-sm rounded-full" @click="onUpdateBtn()">
+        Update Now
       </CommonButton>
     </div>
   </div>
