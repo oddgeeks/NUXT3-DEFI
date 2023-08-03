d27f4f56dcfca66da53b3cd2379c0c7f98f3b42c
oddgeeks <oddgeeks.dev@gmail.com>
2023-08-03 22:52:43 +0300

    fix: contact merge

--- composables/useContact.ts
+++ composables/useContact.ts
@@ -142,6 +142,24 @@ export function useContacts() {
     }
   }
 
+  function getContactNameByAddress(address: string, ownerName = 'You') {
+    if (!address)
+      return ''
+
+    const contact = safeContacts.value.find(
+      contact => getAddress(contact.address) === getAddress(address),
+    )
+
+    if (contact) {
+      if (contact.owner)
+        return ownerName
+
+      return contact.name
+    }
+
+    return ''
+  }
+
   return {
     ownerContact,
     safeContacts,
@@ -152,5 +170,6 @@ export function useContacts() {
     transferCounts,
     getSentTimes,
     migrateOldContacts,
+    getContactNameByAddress,
   }
 }
