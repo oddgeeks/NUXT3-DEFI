d24a3f538d9444eaaa97a96124c3041e98a99643
oddgeeks <oddgeeks.dev@gmail.com>
2023-08-15 22:41:23 +0300

    fix: remove type

--- stores/tokens.ts
+++ stores/tokens.ts
@@ -13,7 +13,6 @@ export interface IToken {
   coingeckoId: string | null
   sparklinePrice7d: number[]
   isCustomToken?: boolean
-  notsupported?: boolean
 }
 
 export const useTokens = defineStore('tokens', () => {
