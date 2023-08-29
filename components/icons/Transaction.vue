0c1094eb0f0f3e0d83530bc1c6fdae1b7e8a2039
Sami <xcitic@gmail.com>
2023-08-29 15:40:53 +0200

    Transaction component with slot icons

---
+++ components/icons/Transaction.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps({
+  bgGreen: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+const bgClass = computed(() => props.bgGreen ? 'bg-primary' : '')
+
+</script>
+
+<template>
+  <div class="w-10 h-10 rounded-full items-center flex justify-center shrink-0"
+    :class="bgClass"
+  >
+    <slot name="icon" />
+  </div>
+</template>
