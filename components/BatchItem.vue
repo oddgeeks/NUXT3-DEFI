f4bb9cddbbba5c7ac343b1cc80afe043f73b8aaa
oddgeeks <oddgeeks.dev@gmail.com>
2023-09-13 17:42:26 +0300

    fix: pointer events add

--- components/BatchItem.vue
+++ components/BatchItem.vue
@@ -41,10 +41,10 @@ function formatValue(i: string) {
             </span>
           </div>
           <div class="ml-auto flex items-center gap-2">
-            <button type="button" @click="$emit('editBatch', index)">
+            <button class="pointer-events-auto" type="button" @click="$emit('editBatch', index)">
               <SvgoPencil class="text-slate-400 w-3 h-3" />
             </button>
-            <button type="button" @click="$emit('deleteBatch', index)">
+            <button class="pointer-events-auto" type="button" @click="$emit('deleteBatch', index)">
               <SvgoDelete class="text-slate-400 w-3 h-3" />
             </button>
             <SvgoChevronDown v-if="expandable" class="group-open:rotate-180 text-slate-400 w-4 h-4" />
