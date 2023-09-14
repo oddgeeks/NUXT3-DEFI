505d45525e24d5d256675bf4b96ce6c107f2ccb6
oddgeeks <oddgeeks.dev@gmail.com>
2023-09-14 16:01:23 +0300

    fix: add alert

--- pages/transaction-builder.vue
+++ pages/transaction-builder.vue
@@ -460,6 +460,10 @@ watch(mode, async (newMode, oldMode) => {
 
 <template>
   <div class="flex-1 relative flex flex-col gap-5">
+    <div class="sm:hidden text-sm text-orange gap-2 flex">
+      <SvgoInfo2 class="shrink-0 mt-1.5" />
+      Transaction builder is not supported on mobile devices
+    </div>
     <ul class="dark:bg-gray-850 bg-slate-50 flex w-fit sm:justify-normal justify-center font-medium text-sm p-1.5 rounded-5 sm:rounded-10">
       <li v-for="item in modes" :key="item.value">
         <button :class="mode === item.value ? 'dark:bg-slate-800 bg-slate-150' : 'text-slate-400'" class="whitespace-nowrap flex items-center gap-2.5 px-6 py-2.5 rounded-2xl sm:rounded-7.5" @click="mode = item.value">
