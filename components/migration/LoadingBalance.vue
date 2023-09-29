12081494f4d3674f8f39abb38d745879b933c081
Felipe <felipelira1908@hotmail.com>
2023-09-29 13:53:18 -0300

    improvement: better loading skeleton

--- components/migration/LoadingBalance.vue
+++ components/migration/LoadingBalance.vue
@@ -2,8 +2,11 @@
   <div class="w-full h-[77px] p-5 flex items-center justify-between border-b-[1px] last:border-b-[0px] border-slate-750">
     <div class="flex items-center">
       <div class="w-[30px] h-[30px] rounded-full loading-box" />
-      <div class="ml-[10px] w-[120px] h-[30px] rounded-full loading-box" />
+      <div class="ml-[10px]">
+        <div class="w-[50px] h-[18px] mb-0.5 rounded-full loading-box" />
+        <div class="w-[100px] h-[16px] rounded-full loading-box" />
+      </div>
     </div>
-    <div class="w-[120px] h-[30px] rounded-full loading-box" />
+    <div class="w-[120px] h-[20px] rounded-full loading-box" />
   </div>
 </template>
--- components/migration/LoadingMultisigWallet.vue
+++ components/migration/LoadingMultisigWallet.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full p-5 flex justify-between dark:bg-gray-850 bg-slate-150 border dark:border-slate-800 border-slate-50 rounded-2xl">
+  <div class="w-full px-4 py-[14px] flex justify-between dark:bg-gray-850 bg-slate-150 border dark:border-slate-800 border-slate-50 rounded-2xl">
     <div>
       <div class="h-[14px] w-[70px] mb-2.5 rounded-full loading-box" />
       <div class="h-[18px] w-[120px] mb-[6px] rounded-full loading-box" />
