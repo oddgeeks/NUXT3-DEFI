824da75c5b1b422ec417795d92394a54793ca0e3
oddgeeks <oddgeeks.dev@gmail.com>
2023-10-19 16:31:10 +0300

    fix: remove confirmation address and add another label

--- components/modals/Mfa/ActivateBackupSigner.vue
+++ components/modals/Mfa/ActivateBackupSigner.vue
@@ -21,10 +21,6 @@ const { handleSubmit, meta } = useForm({
           return false
         }
       }),
-    confirmAddress: yup
-      .string()
-      .required()
-      .oneOf([yup.ref('address')], 'Addresses must match'),
     confirm: yup.boolean().required().equals([true]),
   }),
 })
@@ -39,8 +35,8 @@ function validateAddress(value: any) {
 }
 
 const { value: address, errorMessage } = useField<string>('address')
-const { value: confirmAddress, errorMessage: confirmErrorMessage } = useField<string>('confirmAddress')
 const { value: confirm } = useField<boolean>('confirm')
+const { value: confirm2 } = useField<boolean>('confirm-2')
 
 const onSubmit = handleSubmit(async () => {
   emits('destroy')
@@ -56,21 +52,26 @@ const onSubmit = handleSubmit(async () => {
 
     <form class="flex flex-col gap-5" @submit="onSubmit">
       <div class="flex flex-col gap-2.5">
-        <label for="input-address" class="text-sm font-medium leading-5">Enter address</label>
+        <label for="input-address" class="text-sm font-medium leading-5">Enter EOA address</label>
         <CommonInput v-model="address" :error-message="errorMessage" placeholder="Signer address" autofocus name="address" />
       </div>
-      <div class="flex flex-col gap-2.5">
-        <label for="input-confirm-address" class="text-sm font-medium leading-5">Confirm address</label>
-        <CommonInput v-model="confirmAddress" :error-message="confirmErrorMessage" placeholder="Signer address" name="confirm-address" />
-      </div>
 
-      <label class="mt-2.5 flex cursor-pointer gap-2.5 text-xs" for="input-confirm">
-        <input id="input-confirm" v-model="confirm" name="confirm" class="peer sr-only" type="checkbox">
-        <SvgoCheckCircle class="svg-circle darker peer-checked:success-circle mt-0.5 h-5 w-5 shrink-0 cursor-pointer text-slate-400" />
-        <span :class="!confirm ? 'text-slate-500' : ''">
-          I confirm that i want to add the above address as my backup signer.
-        </span>
-      </label>
+      <div class="flex flex-col gap-2.5">
+        <label class="mt-2.5 flex cursor-pointer gap-2.5 text-xs" for="input-confirm">
+          <input id="input-confirm" v-model="confirm" name="confirm" class="peer sr-only" type="checkbox">
+          <SvgoCheckCircle class="svg-circle darker peer-checked:success-circle mt-0.5 h-5 w-5 shrink-0 cursor-pointer text-slate-400" />
+          <span :class="!confirm ? 'text-slate-500' : ''">
+            I confirm that I want to add the above address as my backup signer.
+          </span>
+        </label>
+        <label class="mt-2.5 flex cursor-pointer gap-2.5 text-xs" for="input-confirm-2">
+          <input id="input-confirm-2" v-model="confirm2" name="confirm-2" class="peer sr-only" type="checkbox">
+          <SvgoCheckCircle class="svg-circle darker peer-checked:success-circle mt-0.5 h-5 w-5 shrink-0 cursor-pointer text-slate-400" />
+          <span :class="!confirm2 ? 'text-slate-500' : ''">
+            I confirm that this address is an EOA address and not an Avocado address.
+          </span>
+        </label>
+      </div>
       <div class="flex gap-4">
         <CommonButton class="flex-1 justify-center" color="white" size="lg">
           Close
