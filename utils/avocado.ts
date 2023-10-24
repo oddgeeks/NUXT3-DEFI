c262f4446038ebdc6226f47170737e0c41fac2d0
oddgeeks <oddgeeks.dev@gmail.com>
2023-10-24 21:53:55 +0300

    refactor: signer

--- utils/avocado.ts
+++ utils/avocado.ts
@@ -12,6 +12,9 @@ export const avoChainName = isProd
 export const AVO_PROD_MULTISIG_FORWARDER_ADDR = '0x46978CD477A496028A18c02F07ab7F35EDBa5A54'
 export const AVO_STAGING_MULTISIG_FORWARDER_ADDR = '0x6f444b80f944FA5Ce3f74cC0e4e1371844B5F91e'
 
+export const stagingInstadappSigner = '0x2D8EE1C03b88b4b72B1c05Dab218EE1982147602'
+export const prodInstadappSigner = '0x2D8EE1C03b88b4b72B1c05Dab218EE1982147602'
+
 export const multisigForwarderProxyAddress = isProd
   ? AVO_PROD_MULTISIG_FORWARDER_ADDR
   : AVO_STAGING_MULTISIG_FORWARDER_ADDR
@@ -31,8 +34,6 @@ export const avoOnboardURL = isProd
   : 'https://onboard.avocado.instad.app'
 
 export const zerionApiURL = 'https://api.zerion.io/v1'
-
-export const instadappSigner = '0x2D8EE1C03b88b4b72B1c05Dab218EE1982147602'
 
 export const incorrectAddress = '0x0000000000000000000000000000000000000000'
 export const ethAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
@@ -70,3 +71,4 @@ export const bannedDapps = ['https://www.orbiter.finance', 'https://mirror.xyz',
 export const bridgeDisabledChains = [250, 122]
 export const swapDisabledChains = [122]
 export const multisigURL = isProd ? multisigURLProd : multisigURLStaging
+export const instadappSigner = isProd ? prodInstadappSigner : stagingInstadappSigner
