88d356144a90be78b74d06e0b58ee9b78669f09c
oddgeeks <oddgeeks.dev@gmail.com>
2023-11-07 16:27:25 +0300

    wip

--- components/Web3Button.vue
+++ components/Web3Button.vue
@@ -24,6 +24,8 @@ const router = useRouter()
 const open = ref(false)
 const hovered = ref(false)
 
+const [DefineTemplate, DisconnectButton] = createReusableTemplate()
+
 const isActualActive = computed(() => {
   if (trackingAccount.value)
     return true
@@ -62,6 +64,20 @@ function userSignOut() {
 </script>
 
 <template>
+  <DefineTemplate>
+    <button
+      class="flex h-7.5 w-7.5 items-center justify-center overflow-hidden rounded-full bg-slate-150 dark:bg-gray-900"
+      aria-label="Close Connection"
+      @click="closeConnection"
+      @mouseenter="hovered = true"
+      @mouseleave="hovered = false"
+    >
+      <div class="absolute overflow-hidden">
+        <PowerOffSVG v-if="hovered" class="pointer-events-none h-12 w-12" />
+        <PowerOnSVG v-else class="pointer-events-none h-12 w-12" />
+      </div>
+    </button>
+  </DefineTemplate>
   <CommonButton v-show="!isActualActive" :class="buttonClass" size="lg" @click="openWeb3Modal">
     Connect
   </CommonButton>
@@ -91,88 +107,38 @@ function userSignOut() {
 
       <div v-else class="loading-box h-5 w-20 rounded-5" />
 
-      <span
-        class="flex h-[26px] w-[26px] items-center justify-center rounded-full bg-primary text-white"
-      ><PlusSVG />
+      <span class="flex h-[26px] w-[26px] items-center justify-center rounded-full bg-primary text-white">
+        <PlusSVG />
       </span>
     </button>
     <template v-if="!hideEOA">
-      <Popover
-        as="div" class="relative z-30 flex items-center gap-4"
-      >
-        <PopoverButton class="relative flex items-center justify-between gap-x-2.5 rounded-7.5 bg-slate-100 px-4.5 py-2.5 leading-5 dark:bg-gray-900 sm:px-4 sm:py-3">
-          <div class="flex gap-[14px]">
-            <div class="flex items-center gap-2.5">
-              <div v-if="connectedProvider">
-                <component :is="connectedProvider.logo" class="h-7.5 w-7.5 sm:h-6 sm:w-6" />
-              </div>
-              <div class="flex flex-col items-start gap-[6px]">
-                <span>{{ addressLabel }}</span>
-              </div>
+      <button class="relative flex items-center justify-between gap-x-2.5 rounded-7.5 bg-slate-100 px-4.5 py-2.5 leading-5 dark:bg-gray-900 sm:px-4 sm:py-3">
+        <div class="flex gap-[14px]">
+          <component :is="connectedProvider.logo" v-if="connectedProvider" class="h-7.5 w-7.5 sm:h-6 sm:w-6" />
+        </div>
+      </button>
+
+      <div class="flex w-full justify-between p-5">
+        <div class="flex gap-[14px]">
+          <div class="flex items-center gap-2.5">
+            <div v-if="connectedProvider">
+              <component :is="connectedProvider.logo" class="h-7.5 w-7.5 sm:h-9 sm:w-9" />
+            </div>
+            <div class="flex flex-col items-start gap-[6px]">
+              <span class="text-xs font-medium leading-[10px] text-gray-500">Owner's Address</span>
+              <span class="text-lg font-semibold leading-5">{{ addressLabel }}</span>
             </div>
           </div>
 
-          <SvgoChevronDown class="shrink-0" />
-        </PopoverButton>
-        <transition
-          enter-active-class="transition duration-100 ease-out"
-          enter-from-class="transform scale-95 opacity-0"
-          enter-to-class="transform scale-100 opacity-100"
-          leave-active-class="transition duration-75 ease-out"
-          leave-from-class="transform scale-100 opacity-100"
-          leave-to-class="transform scale-95 opacity-0"
-        >
-          <PopoverPanel
-            v-slot="{ close }"
-            as="div"
-            class="absolute right-0 top-0 flex flex-col justify-between gap-x-2.5 rounded-[25px] bg-slate-100 leading-5 ring-1 ring-slate-150 dark:bg-gray-850 dark:ring-slate-750 sm:w-[440px]"
+          <button
+            class="flex h-7.5 w-7.5 items-center justify-center overflow-hidden rounded-full bg-slate-150 dark:bg-gray-900"
+            aria-label="Copy EOA"
           >
-            <div class="flex w-full justify-between p-5">
-              <div class="flex gap-[14px]">
-                <div class="flex items-center gap-2.5">
-                  <div v-if="connectedProvider">
-                    <component :is="connectedProvider.logo" class="h-7.5 w-7.5 sm:h-9 sm:w-9" />
-                  </div>
-                  <div class="flex flex-col items-start gap-[6px]">
-                    <span class="text-xs font-medium leading-[10px] text-gray-500">Owner's Address</span>
-                    <span class="text-lg font-semibold leading-5">{{ addressLabel }}</span>
-                  </div>
-                </div>
-
-                <button
-                  class="flex h-7.5 w-7.5 items-center justify-center overflow-hidden rounded-full bg-slate-150 dark:bg-gray-900"
-                  aria-label="Copy EOA"
-                >
-                  <Copy :text="trackingAccount || account" :icon-only="true" />
-                </button>
-
-                <button
-                  class="flex h-7.5 w-7.5 items-center justify-center overflow-hidden rounded-full bg-slate-150 dark:bg-gray-900"
-                  aria-label="Close Connection"
-                  @click="closeConnection"
-                  @mouseenter="hovered = true"
-                  @mouseleave="hovered = false"
-                >
-                  <div class="absolute overflow-hidden">
-                    <PowerOffSVG v-if="hovered" class="pointer-events-none h-12 w-12" />
-                    <PowerOnSVG v-else class="pointer-events-none h-12 w-12" />
-                  </div>
-                </button>
-              </div>
-              <button
-                class="flex h-7.5 w-7.5 items-center justify-center rounded-full bg-slate-150 dark:bg-gray-900"
-                aria-label="Close EOA"
-                @click.stop="close"
-              >
-                <SvgoX />
-              </button>
-            </div>
-            <div class="border-t border-slate-150 px-5 pb-5 pt-4 dark:border-slate-750">
-              <WalletItemList />
-            </div>
-          </PopoverPanel>
-        </transition>
-      </Popover>
+            <Copy :text="trackingAccount || account" :icon-only="true" />
+          </button>
+          <DisconnectButton />
+        </div>
+      </div>
     </template>
   </div>
 </template>
