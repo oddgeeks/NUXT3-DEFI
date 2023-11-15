e01285f90e2448e7b1ab47b11d065918ef596021
oddgeeks <oddgeeks.dev@gmail.com>
2023-11-15 18:07:59 +0300

    fix: style

--- components/banner/NewVersion.client.vue
+++ components/banner/NewVersion.client.vue
@@ -1,6 +1,8 @@
+<script setup lang="ts"></script>
+
 <template>
   <Transition name="slide-fade">
-    <div v-if="$pwa.needRefresh" class="bg-[#4CA05460]px-5 fixed bottom-[50px] left-1/2 z-[100] flex w-full -translate-x-1/2 items-center gap-[15px] rounded-5 py-4 pr-7.5 backdrop-blur sm:max-w-[400px]">
+    <div v-if="$pwa.needRefresh" class="fixed bottom-[50px] left-1/2 z-[100] flex w-full -translate-x-1/2 items-center gap-[15px] rounded-5 bg-primary px-5 py-4 pr-7.5 backdrop-blur sm:max-w-[400px]">
       <SvgoGear />
       <p class="flex-1 text-xs leading-5 ">
         New version available.
