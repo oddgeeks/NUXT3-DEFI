6ba83f32daba50197e31ccd88a4677c32e3c0385
oddgeeks <oddgeeks.dev@gmail.com>
2023-11-29 10:31:05 +0300

    refactor: update style

--- components/banner/Migration.vue
+++ components/banner/Migration.vue
@@ -4,22 +4,22 @@ const { hideMigrationBanner } = useBanner()
 
 <template>
   <div
-    class="mb-4 flex w-full flex-col items-center justify-center gap-[15px] rounded-5 bg-primary/10 p-4 text-center text-sm text-primary sm:flex-row"
+    class="relative mb-4 flex w-full flex-col items-center justify-center rounded-5 bg-primary/10 p-4 text-center text-sm text-primary sm:flex-row"
   >
-    <div class="inline-flex items-center gap-2">
+    <div class="flex items-center gap-[15px]">
       <SvgoEyes />
       <span class="self-center text-xs">
         Migrate your balances to Avocado Personal to get the latest updates
       </span>
+      <CommonButton as="NuxtLink" to="/migration" class="flex h-7.5 justify-center !px-4 text-xs">
+        Migrate
+      </CommonButton>
     </div>
-    <CommonButton as="NuxtLink" to="/migration" class="flex h-7.5 justify-center !px-4 text-xs">
-      Migrate
-    </CommonButton>
     <button
-      class="flex h-5 w-5 items-center justify-center rounded-full bg-white/20"
+      class="absolute right-5 flex h-5 w-5 items-center justify-center rounded-full bg-white/20"
       @click="hideMigrationBanner"
     >
-      <SvgoX class="text-white" />
+      <SvgoX class="h-2.5 w-2.5 text-white" />
     </button>
   </div>
 </template>
