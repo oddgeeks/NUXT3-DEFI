87e22a782ec914359b66372326efbbee14e097a4
oddgeeks <oddgeeks.dev@gmail.com>
2023-12-11 11:12:05 +0300

    feat: add announcement banner

---
+++ components/banner/Announcement.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { hideAnnouncementBanner } = useBanner()
+</script>
+
+<template>
+  <div class="sticky top-0 z-[41] flex h-7.5 w-full items-center justify-center gap-1 bg-[#08884e] text-center text-xs">
+    <span>
+      Announcing support for Scroll, opBNB and Fuse on Avocado
+    </span>
+    <button class="underline" type="button" @click="openSupportedNetworks()">
+      View all supported chains
+    </button>
+    <button class="absolute right-4" type="button" @click="hideAnnouncementBanner()">
+      <SvgoX class="h-3 w-3" />
+    </button>
+  </div>
+</template>
