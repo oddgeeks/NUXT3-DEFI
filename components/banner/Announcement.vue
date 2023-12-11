695eba2a0693f28ae1725462b2673cbeab715699
oddgeeks <oddgeeks.dev@gmail.com>
2023-12-11 11:39:46 +0300

    fix: adjust mobile

--- components/banner/Announcement.vue
+++ components/banner/Announcement.vue
@@ -3,14 +3,14 @@ const { hideAnnouncementBanner } = useBanner()
 </script>
 
 <template>
-  <div class="sticky top-0 z-[41] flex h-7.5 w-full items-center justify-center gap-1 bg-[#08884e] text-center text-xs">
+  <div class="sticky top-0 z-[41] inline-flex w-full flex-col items-center justify-center gap-1 bg-[#08884e] p-2 text-center text-xs sm:top-0 sm:h-7.5 sm:flex-row sm:p-0">
     <span>
       Announcing support for Scroll, opBNB and Fuse on Avocado
     </span>
     <button class="underline" type="button" @click="openSupportedNetworks()">
       View all supported chains
     </button>
-    <button class="absolute right-4" type="button" @click="hideAnnouncementBanner()">
+    <button class="absolute right-2 sm:right-4" type="button" @click="hideAnnouncementBanner()">
       <SvgoX class="h-3 w-3" />
     </button>
   </div>
