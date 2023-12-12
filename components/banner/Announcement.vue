53eb3d5efbd73bd41f3bf933376554d6818ea967
Mert <45617686+yulafezmesi@users.noreply.github.com>
2023-12-12 16:42:54 +0300

    Update Announcement.vue

--- components/banner/Announcement.vue
+++ components/banner/Announcement.vue
@@ -5,7 +5,7 @@ const { hideAnnouncementBanner } = useBanner()
 <template>
   <div class="sticky top-0 z-[41] inline-flex w-full flex-col items-center justify-center gap-1 bg-[#08884e] p-2 text-center text-xs sm:top-0 sm:h-7.5 sm:flex-row sm:p-0">
     <span>
-      Announcing support for Scroll, opBNB and Fuse on Avocado
+      We are excited to announce support for Scroll, opBNB, and Fuse on Avocado!
     </span>
     <button class="underline" type="button" @click="openSupportedNetworks()">
       View all supported chains
