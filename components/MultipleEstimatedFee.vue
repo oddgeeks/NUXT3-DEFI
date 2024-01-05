88f6efa371cd2f4674e5ebc96a12f8d7892ff02f
oddgeeks <oddgeeks.dev@gmail.com>
2024-01-05 16:25:11 +0300

    fix: hide network info incase item eq 1

--- components/MultipleEstimatedFee.vue
+++ components/MultipleEstimatedFee.vue
@@ -10,27 +10,30 @@ defineProps<{
 <template>
   <ul v-if="data || error" class="flex flex-col gap-4 rounded-2xl bg-gray-850 py-[14px]">
     <template v-if="data?.length">
-      <li v-for="item in data" :key="item.chainId" class="flex items-center justify-between px-4 font-medium">
-        <div class="flex items-center gap-2.5 text-xs">
-          <ChainLogo class="h-4.5 w-4.5" :chain="item.chainId" />
-          {{ chainIdToName(item.chainId) }}
-        </div>
-        <p class="inline-flex items-center gap-2 text-xs font-medium">
-          <img
-            class="h-[18px] w-[18px]"
-            width="18"
-            height="18"
-            src="https://cdn.instadapp.io/icons/tokens/usdc.svg"
-          >
+      <template v-if="data.length > 1">
+        <li v-for="item in data" :key="item.chainId" class="flex items-center justify-between px-4 font-medium">
+          <div class="flex items-center gap-2.5 text-xs">
+            <ChainLogo class="h-4.5 w-4.5" :chain="item.chainId" />
+            {{ chainIdToName(item.chainId) }}
+          </div>
+          <p class="inline-flex items-center gap-2 text-xs font-medium">
+            <img
+              class="h-[18px] w-[18px]"
+              width="18"
+              height="18"
+              src="https://cdn.instadapp.io/icons/tokens/usdc.svg"
+            >
 
-          {{ item?.formattedAmountAfterDiscount }} USDC
-        </p>
-      </li>
-      <li class="border-t  border-slate-800 px-4 pt-3">
+            {{ item?.formattedAmountAfterDiscount }} USDC
+          </p>
+        </li>
+      </template>
+      <li :class="data?.length > 1 ? 'pt-3' : 'border-slate-800'" class="px-4">
         <div class="flex w-full justify-between">
           <p class="flex items-center gap-2.5">
             <SvgoGas class="text-slate-400" />
-            Total gas fees
+            <span v-if="data?.length > 1">Total gas fees</span>
+            <span v-else>Gas fees</span>
           </p>
           <p class="inline-flex items-center gap-2">
             <img
